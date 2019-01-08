5de41eeeac8185982c40eb10fc0126e572afa401
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2019-01-08 00:29:52 -0300

    muestra conciertos en home y siembra para 2018

--- app/models/encuentro.rb
+++ app/models/encuentro.rb
@@ -14,6 +14,10 @@ class Encuentro < ApplicationRecord
     charlas.group(:fecha).select(:fecha)
   end
 
+  def conciertos_por_dia
+    charlas.group(:fecha).select(:fecha)
+  end
+
   def fotos_charlas
     ary = Array.new
     charlas.each do |c|
