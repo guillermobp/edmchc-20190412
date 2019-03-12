5acbe4c9d6ea1d89dfed1fff782c066649bb50a6
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2019-03-12 06:35:06 -0300

    :recycle: anida recursos del encuentro

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   root 'home#index'
 
-  resources :exponentes, :ensayos, :encuentros, :coordinadores, :charlas, :conciertos
+  get 'admin', to: 'admin#index'
+
+  resources :encuentros, shallow: true do
+    resources :exponentes,
+              :ensayos,
+              :coordinadores,
+              :charlas,
+              :conciertos
+  end
 
   get '/charlas/:id/fotos', to: 'charlas#fotos', as: :charla_fotos
   get '/conciertos/:id/fotos', to: 'conciertos#fotos', as: :concierto_fotos
-
 end
