177a173f7cbf62b43bd8fc3d382c8afcb2cbdb7e
Guillermo Barriga Placencia <guillermo@bitbox.cl>
2019-04-02 06:56:32 -0300

    :bug: formatea fechas y horas de conciertos ssi existen datos

--- app/models/concierto.rb
+++ app/models/concierto.rb
@@ -6,15 +6,15 @@ class Concierto < ApplicationRecord
   default_scope { order(:fecha, :hora) }
 
   def fecha_display
-    I18n.l(fecha, format: :day_name_day_number_month_name)
+    fecha_display ? I18n.l(fecha, format: :day_name_day_number_month_name) : ''
   end
 
   def fecha_corta
-    fecha.strftime('%d/%m/%y')
+    fecha ? fecha.strftime('%d/%m/%y') : ''
   end
 
   def hora_display
-    I18n.l(hora, format: :short)
+    hora ? I18n.l(hora, format: :short) : ''
   end
 
   def nombre_compositor
